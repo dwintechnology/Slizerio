1c2c6a8ba6480d70ef13b10eb7d5ed1b50559917
Jorj99 <jorj.hambaryan.dwin@gmail.com>
2022-02-14 23:02:53 +0400

    buttons

--- movies/src/components/test.jsx
+++ movies/src/components/test.jsx
@@ -2,56 +2,68 @@ import { useEffect, useState } from "react"
 
 function Test() {
     const [data, setData] = useState();
-    let [k, setK]= useState()
+    const [itemsTitle, setItemsTitle] = useState();
+    let buttonsArr = [];
+
+
     useEffect(() => {
         async function foo() {
-            // let a = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=e81b5d12d52c0de91f235d90e155683c&language=en-US&page=14')
-            // let b = await a.json()
-            // setData(b)
-            // console.log(b)
-            fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=e81b5d12d52c0de91f235d90e155683c&language=en-US&page=14')
-            .then((a)=>{
-                 
-                return a.json()
-            })
-            .then((b)=>{
-                setData(b)
-                // console.log(b)
-            })
-            
+            fetch('https://api.themoviedb.org/3/movie/popular?api_key=e81b5d12d52c0de91f235d90e155683c&language=en-US&page=1')
+                .then((a) => { return a.json() })
+                .then((b) => { setData(b) })
         }
         foo()
-       
-        if(data !== undefined){
-            console.log(data.results)
-            let o = data.results.map((p)=>{
-             return (
-                 <div>
-                     <h1>{p.title}</h1>
-                 </div>
-             )
-           })
-           setK(o)
-        }        
-    },[data])
-    // let [k, setK]= useState()
-    // useEffect(()=>{
-    //     console.log(data.results)
-    //      let o = data.results.map((p)=>{
-    //       return (
-    //           <div>
-    //               <h1>{p.title}</h1>
-    //           </div>
-    //       )
-    //     })
-    //     setK(o)
-    // },[data])
-   
-    
+
+    }, [])
+    useEffect(() => {
+        if (data !== undefined) {
+            console.log(data)
+            let items = data.results.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <h1>{item.title}</h1>
+                    </div>
+                )
+            })
+            setItemsTitle(items)
+        }
+    }, [data])
+
+    // bottom buttons 
+    function crateButtons() {
+        let buttonsArrLength = buttonsArr.length;
+        buttonsArr.length = buttonsArrLength + 10;
+        buttonsArr = buttonsArr.fill(1)
+        buttonsArr = buttonsArr.map((el, index) => { return el + index })
+    }
+    crateButtons()
+
+    let DrawButtons = buttonsArr.map((el, index) => {
+        return (
+            <div key={index} onClick={(e) => {
+                function foo() {
+                    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e81b5d12d52c0de91f235d90e155683c&language=en-US&page=${e.target.textContent}`)
+                        .then((a) => { return a.json() })
+                        .then((b) => { 
+                            let results = b.results
+                            let dataCopy = JSON.parse(JSON.stringify(data))
+                            results.forEach(element => {
+                                dataCopy.results.push(element)
+                            });
+                            console.log(dataCopy);
+                            setData(dataCopy)
+                        })
+                }
+                foo()
+            }}>{el}</div>
+        )
+    })
+    // bottom buttons 
+
     return (
-        <div>  
-            {/* <h1>{data}</h1> */}
-            {k}
+        <div>
+            {itemsTitle}
+            {DrawButtons}
         </div>
     )
 }
