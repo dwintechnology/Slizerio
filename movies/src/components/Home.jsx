09c883118ca9f38667e75a6017eb53a416f06140
SuroBegloyan <suro.begloyan.dwin@gmail.com>
2022-02-17 12:27:32 +0400

    Movies card style fixed

--- movies/src/components/Home.jsx
+++ movies/src/components/Home.jsx
@@ -7,8 +7,7 @@ import { makeStyles } from '@mui/styles';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-
-
+import MoviesCard from "./MoviesCard";
 const useStyle = makeStyles({
     cardStyle: {
         '&.MuiCardMedia-root': {
@@ -19,76 +18,73 @@ const useStyle = makeStyles({
             boxShadow: '0rem 2rem 5rem #00000033',
             transition: 'all 100ms cubic-bezier(0.645, 0.045, 0.355, 1) 0s',
             backgroundColor: 'transparent',
-
         }
     },
-
     cardMainStyle: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         boxShadow: 'unset !important',
-        margin: '50px'
+        margin: '50px',
+        '&:hover': {
+            transform: 'scale(1.03)'
+        },
+    },
+    link: {
+        '&:hover': {
+            backgroundColor: '#37474f',
+            transition: 'all 300ms cubic-bezier(0.645, 0.045, 0.355, 1) 0s',
+        },
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        //transition: 'all 500ms',
     }
-
 }, {
     name: 'Home'
 })
-
-
 function Home({ setObj }) {
     const [data, setData] = useState();
     const [page, setPage] = useState(1);
     const cardStyle = useStyle()
-
-
     function LoadMovie() {
         setPage(page + 1)
         getMovies(page + 1)
     }
-
     function LoadPreviousMovies() {
         setPage(page - 1)
         getMovies(page - 1)
     }
-
     function getMovies(page) {
         fetch(`${constants.API_PATH}/movie/popular?api_key=${constants.API_KEY}c&language=en-US&page=${page}`)
             .then((a) => { return a.json() })
             .then((b) => { setData(b) })
     }
-
     useEffect(() => {
         getMovies(page)
     }, [])
-
     let items = data?.results.map((movie, index) => {
         return (
-            <Grid key={index} item xs={3}>
-                <Card sx={{ maxWidth: 334, maxHeight: 418 }} className={cardStyle.cardMainStyle}>
-                    <Link onClick={() => { setObj(data) }
-                    } to={`/film_About/${index}`}>
-                        <CardMedia
-                            className={cardStyle.cardStyle}
-                            component="img"
-                            image={`${constants.SMALL_IMG_PATH}${movie.poster_path}`}
-                            alt="Paella dish"
-                        />
-                        <CardContent>
-                            <Typography variant="p" color="text.secondary">
-                                {movie.title}
-                            </Typography>
-                        </CardContent>
+            <Grid key={index} item xs={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Card sx={{ borderRadius: '0.8rem', width: 220 * 1.03 }} className={cardStyle.cardMainStyle}>
+                    <Link
+                        className={cardStyle.link}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            setObj(data)
+                        }}
+                        to={`/film_About/${index}`}
+                    >
+                        <MoviesCard movie={movie} />
                     </Link>
                 </Card>
             </Grid>
         )
     })
-
-
     return (
         <div>
-            <Grid container columnSpacing={5}>
+            <Grid container columnSpacing={1}>
                 {items}
             </Grid>
             <button onClick={LoadPreviousMovies}>{`Page ${page - 1}`}</button>
@@ -96,5 +92,4 @@ function Home({ setObj }) {
         </div>
     )
 }
-
 export default Home
---
+++ movies/src/components/MoviesCard.jsx
@@ -0,0 +1,58 @@
+import { makeStyles } from '@mui/styles';
+import CardMedia from '@mui/material/CardMedia';
+import constants from "../utils/constants";
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+
+const useStyle = makeStyles({
+    cardImageStyle: {
+        '&.MuiCardMedia-root': {
+            '&:hover': {
+                transform: 'scale(1.03)'
+            },
+            width: "220px",
+            height: "335px",
+            objectFit: 'cover',
+            borderRadius: '0.8rem',
+            boxShadow: '0rem 2rem 5rem #00000033',
+            transition: 'all 300ms cubic-bezier(0.645, 0.045, 0.355, 1) 0s',
+            backgroundColor: 'transparent',
+        }
+    },
+    cardContent: {
+        '&.MuiCardContent-root': {
+            width: 180
+        }
+    },
+    cardHover: {
+        // transition: 'all 300ms',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: 220 * 1.03
+    },
+}, {
+    name: 'MoviesCard'
+})
+
+function MoviesCard({ movie }) {
+    const cardStyle = useStyle();
+    return (
+        <div className={cardStyle.cardHover}>
+            <CardMedia
+                className={cardStyle.cardImageStyle}
+                component="img"
+                image={`${constants.SMALL_IMG_PATH}${movie.poster_path}`}
+                alt="Paella dish"
+            />
+            <CardContent className={cardStyle.cardContent}>
+                <Typography variant="p" color="text.secondary">
+                    {movie.title}
+                </Typography>
+            </CardContent>
+            <Rating name="no-value" value={null} style={{ marginBottom: 16 }} />
+        </div>
+    )
+}
+export default MoviesCard;
